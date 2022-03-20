11b2dcda7f9f714e511cce799a3ab6782c78c66f
prateeksinghsaini <prateek1901267@gmail.com>
2022-03-20 11:42:32 +0530

    replaced

--- javascript.js
+++ javascript.js
@@ -1,10 +1,11 @@
 let trackArt = document.querySelector("#trackArt");
+let coverImg = document.querySelector("#coverImg");
 let trackName = document.querySelector("#trackName");
 let artistName = document.querySelector("#artistName");
 
 let playPause = document.querySelector("#play");
 let prevBtn = document.querySelector("#previous");
-let netBtn = document.querySelector("#next");
+let nextBtn = document.querySelector("#next");
 
 let seekSlider = document.querySelector("#myProgressBar");
 let currentTime = document.querySelector("#currentTime");
@@ -15,8 +16,10 @@ let volumeUp = document.querySelector("#plus");
 let volumedown = document.querySelector("#minus");
 
 let songItems = document.querySelector(".songItem");
-let songName = document.querySelector(".songName"); 
-////////
+let songName = Array.from(document.querySelector(".songName")); 
+
+//////// playlistbox //////////////////////////////////////////////////////////
+
 let playlistStatus = false;
 
 let openPlayList = function() {
@@ -31,18 +34,37 @@ let openPlayList = function() {
         playlistStatus = false
     }
 };
-///////
+
+/////// songsarry ////////////////////////////////////////////////////////////
+
 let songIndex = 0;
 let songs = [
-    {trackArtPath: "covers/img1.jpg", trackName: "STAY", filePath: "songs/song1.mp3", artistName: "The Kid LAROI, Justin Bieber"},
-    {trackArtPath: "covers/img2.jpg", trackName: "Sunflower", filePath: "songs/song2.mp3", artistName: "Post Malone and Swae Lee"},
-    {trackArtPath: "covers/img3.jpg", trackName: "BeFoUr", filePath: "songs/song3.mp3", artistName: "Zayn"}
+    {trackArt: "covers/STAY.jpg",
+    trackName: "STAY",
+    artistName: "The Kid LAROI, Justin Bieber"},
+    
+    {trackArt: "covers/Sunflower.jpg",
+    trackName: "Sunflower",
+    artistName: "Post Malone and Swae Lee"},
+    
+    {trackArt: "covers/BeFoUr.jpg",
+    trackName: "BeFoUr",
+    artistName: "Zayn"}
+];
+
+let imgIndex = 0;
+let imgs = [
+    "covers/STAY.jpg",
+    "covers/Sunflower.jpg",
+    "covers/BeFoUr.jpg"
 ];
 
 let audioElement = new Audio("songs/song1.mp3");
-////////
-playPause.addEventListener("click", ()=>{
-    if(audioElement.paused || audioElement.currentTime<=0){
+
+//////// playbutton /////////////////////////////////////////////////////////
+
+let playsong = function(songs) {
+     if(audioElement.paused){
         audioElement.play();
         playPause.classList.remove("fa-play");
         playPause.classList.add("fa-pause");
@@ -53,8 +75,12 @@ playPause.addEventListener("click", ()=>{
         playPause.classList.add("fa-play");
         document.querySelector("#play").title = "play";
     }
-});
-/////////
+}
+
+playPause.addEventListener("click", playsong);
+
+///////// progressbar //////////////////////////////////////////////////////
+
 audioElement.addEventListener("timeupdate", ()=>{
     progress = parseInt((audioElement.currentTime/audioElement.duration)* 100)
     seekSlider.value = progress;
@@ -62,7 +88,9 @@ audioElement.addEventListener("timeupdate", ()=>{
 seekSlider.addEventListener("change", ()=>{
     audioElement.currentTime = seekSlider.value * audioElement.duration/100;
 });
-////////
+
+//////// voluebar /////////////////////////////////////////////////////////
+
 volumeSlider.addEventListener("mousemove", ()=>{
     audioElement.volume = volumeSlider.value / 100;
 });
@@ -71,17 +99,36 @@ volumeUp.addEventListener("click", ()=>{
 
 });
 
+//////// next prev /////////////////////////////////////////////////////////
 
+let loadSong = (songs) => {
+    trackName.textContent = songs.trackName;
+    artistName.textContent = songs.artistName;
+    audioElement.src = "songs/" + songs.trackName + ".mp3";
+    artistName.textContent = songs.artistName;
+    coverImg.src = "covers/" + songs.trackName + ".jpg";
+};
+loadSong(songs[0]);
 
+let nextSong = () => {
+    songIndex = (songIndex + 1) % songs.length;
+    loadSong(songs[songIndex]);
+    playsong();
+};
+/*let prevSong = () => {
+    songIndex = (((songIndex - 1) % songs.length) + songs.length) % songs.length;
+    loadSong(songs[songIndex]);
+    playsong();
+};*/
 
+nextBtn.addEventListener("click", nextSong);
+nextBtn.addEventListener("click", prevSong);
 
+////////  auto next /////////////////////////////////////////////////////////
 
-
-
-
-
-/*Array.from(document.querySelector("#playpause")).forEach((element)=>{
-    element.addEventListener("click", (e)=>{
-        console.log(e);
-    })
-});*/
+/*let autoNext = () => {
+    if(audioElement.currentTime = 100){
+    playPause(),
+    nextSong();
+    }
+}*/
